d4695fbb26e40db0894842748c127dac650dcf13
Firat Akkoyun <92083856+Thefirat@users.noreply.github.com>
2023-01-09 10:44:47 +0100

    Footer fixed

---
+++ .history/frontend/src/components/header/Header_20230109102627.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope, faLock, faPhone, faShoppingBag } from '@fortawesome/free-solid-svg-icons'
+import '../../pages/home/home.css';
+import { Link, NavLink } from 'react-router-dom'; 
+const Header = () => {
+  return (
+    <div className='header-row'>
+        <div className="header-top">
+            <span><FontAwesomeIcon icon={faPhone} />+76/0000000</span>
+            <span><FontAwesomeIcon icon={faEnvelope} />mernETecnology@gmail.com</span>
+            
+        </div>
+        <div className="header-menu">
+            <div className="header-logo">
+                <Link to="/"><h2 className="header-logo-title"></h2> </Link>
+            </div>
+
+            <div className="header-nav">
+                <NavLink to="/" activeClassName="active">Home</NavLink>
+                <NavLink to="/shop" activeClassName="active">Shop</NavLink>
+                <NavLink to="/sellers" activeClassName="active">Sellers</NavLink> 
+            </div>
+
+            <div className="header-action">
+                 <Link to="/cart"><FontAwesomeIcon icon={faShoppingBag} />
+                <span className='header-cart-badge'>0</span></Link>
+                 <Link to="/login"><FontAwesomeIcon icon={faLock} />Login</Link>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Header 
